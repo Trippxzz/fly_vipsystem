1cd239d21f4adecf36df2e2ba36ee27c3cc407d8
yoeltripp <cris.ct2003@gmail.com>
2023-03-17 19:05:05 -0300

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -1,11 +1,11 @@
 Config = {}
 
-Config.Vips = {
+Config.Vips = {         --##if you add more types of vips or change their values, remember to change them in bot.js (line 100) as well.
      {
         Icon = "🟫",
         Name = "Bronze",
         Ped = 'notavailable',
-        Cars = 1,
+        Cars = 1,   
         Money = 1000
     },
      {
@@ -18,7 +18,7 @@ Config.Vips = {
      {
         Icon = "🟨",
         Name = "Golden",
-        Ped = 1,
+        Ped = 1,           --if it is a 1, it can have 1 ped
         Cars = 3,
         Money = 3000
     },
@@ -26,7 +26,7 @@ Config.Vips = {
         Icon = "🟦",
         Name = "Diamond",
         Ped = 1,
-        Cars = 5,
+        Cars = 5,           --number of cars you can claim
         Money = 4000
     }
 }
@@ -35,7 +35,7 @@ Config.Cars = {
      {model = "t20", label = "T20" ,category = "Bronze"},
      {model = "komoda", label = "Komoda", category = "Bronze"},
      {model ="zentorno", label = "Zentorno", category ="Silver"},
-     {model = "adder"}, label = "Adder", category ="Silver",
+     {model = "adder", label = "Adder", category ="Silver"},
      {model = "krieger", label = "Krieger", category = "Golden"},
      {model = "xa21", label = "Xa-21", category = "Golden"},
      {model = "toros", label = "Toros", category ="Diamond"},
